3e2e45d29fa189cc0ad37bef08d8e5472a05896f
kewlamogh <amogh_sarma@outlook.com>
2021-07-10 17:51:54 -0700

    set up basics of encoding

---
+++ docs/script.js
@@ -0,0 +1,46 @@
+//...
+
+let textToNumber = {};
+function addKeyToTextToNumber(keyName, keyVal) {
+    textToNumber[keyName.toLowerCase()] = keyVal;
+    textToNumber[keyName.toUpperCase()] = keyVal;
+}
+
+function encode(txt) {
+    let chars = txt.split('');
+    let encoded = [];
+    for (var i in chars) {
+        encoded.push(textToNumber[chars[i]]);
+    }
+    console.log(encoded);
+}
+
+addKeyToTextToNumber('a', 2);
+addKeyToTextToNumber('b', 3);
+addKeyToTextToNumber('c', 4);
+addKeyToTextToNumber('d', 5);
+addKeyToTextToNumber('e', 6);
+addKeyToTextToNumber('f', 7);
+addKeyToTextToNumber('g', 8);
+addKeyToTextToNumber('h', 9);
+addKeyToTextToNumber('i', 10);
+addKeyToTextToNumber('j', 11);
+addKeyToTextToNumber('k', 12);
+addKeyToTextToNumber('l', 13);
+addKeyToTextToNumber('m', 14);
+addKeyToTextToNumber('n', 15);
+addKeyToTextToNumber('o', 16);
+addKeyToTextToNumber('p', 17);
+addKeyToTextToNumber('q', 18);
+addKeyToTextToNumber('r', 19);
+addKeyToTextToNumber('s', 20);
+addKeyToTextToNumber('t', 21);
+addKeyToTextToNumber('u', 22);
+addKeyToTextToNumber('v', 23);
+addKeyToTextToNumber('w', 24);
+addKeyToTextToNumber('x', 25);
+addKeyToTextToNumber('y', 26);
+addKeyToTextToNumber('z', 1);
+addKeyToTextToNumber(' ', '-');
+
+encode('I am evil the dumbo')
